22ea5dddd87f1f2992bc0badd6958f933d8ff9fa
ChandanMayya <chandanmogral@gmail.com>
2022-04-19 16:11:03 +0530

    Not Working

---
+++ 8a.sh
@@ -0,0 +1,18 @@
+m=who | grep "`whoami`" | tr -s " " | cut -d " " -f4 | cut -c 4-5
+h=who | grep "`whoami`" | tr -s " " | cut -d " " -f4 | cut -c 1-2
+ch=`date +%H`
+cm=`date +%M`
+d=$m-$cm 
+echo $d
+if [ $d -lt 0 ]
+then
+	d=`expr $d + 60`
+	echo $d
+fi
+hh=$h-$ch
+echo $hh
+if [ $hh -lt 0 ]
+then
+	hh=`expr $hh + 24`
+	echo $hh
+fi
