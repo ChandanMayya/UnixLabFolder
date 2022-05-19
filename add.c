f38c32d17ad0b363b92281bbd3363a8d60b6b8a3
ChandanMayya <chandanmogral@gmail.com>
2022-05-19 22:43:24 +0530

    maintainance

--- add.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){
-int a,b,sum;
-a=10;
-b=20;
-sum=10+20;
-printf("%d",sum);
-}
-
--- dir/shell.sh
+++
@@ -1,3 +0,0 @@
-e=`ls -l | wc -l`
-echo "tHERE ARE `expr $e - 1`  FILES IN THE DIRECTORY" 
-
