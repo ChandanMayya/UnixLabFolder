1bf532af56a39f584b56336cdf6087c32e7708f3
ChandanMayya <chandanmogral@gmail.com>
2022-06-01 15:27:49 +0530

    8a Multiuser

--- 8a.sh
+++ 8a.sh
@@ -1,17 +1,24 @@
-ch=`date +%H`
-cm=`date +%M`
-
-echo "Users logged in:"
-who | tr -s " " | cut -d " " -f 1 
-echo "Enter the user:"
-read user
-set -- `who | tr -s " " | grep $user | cut -d " " -f 4 | tr ":" " "`
-
-hdiff=`expr $ch - $1`
-mdiff=`expr $cm - $2`
-if [ $mdiff -lt 0 ]
+if [ $# -gt 0 ]
 then
-	hdiff=`expr $hdiff - 1`
-	mdiff=`expr $mdiff + 60`
+	who | grep -w $1 >s
+	if [ $? -eq 0 ]
+	then
+		ch=`date +%H`   #CURRENT TIME
+		cm=`date +%M`
+		set -- `cat s | tr -s " " | cut -d " " -f 4 | tr ":" " "`
+		lh=$1		#LOGIN TIME
+		lm=$2
+		hr=`expr $ch - $lh`
+		mn=`expr $cm - $lm`	#WORKING TIME
+		if [ $mn -lt 0 ]
+		then
+			mn=`expr $mn + 60`
+			hr=`expr $hr - 1`
+		fi
+		echo "Specified user worked $hr : $mn "
+	else	
+		echo "User not Logged in!"
+	fi
+else
+	echo Enter Username as parameter
 fi
-echo "User $user worked for $hdiff Hours - $mdiff Minutes"
